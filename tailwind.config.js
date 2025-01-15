08ac7154ab6144afc60840d8581440f93bf7a0b0
Mary Rebecca Lindsey <maryrebeccalindsey@gmail.com>
2025-01-15 17:53:51 -0600

    change dirs of all files in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./calculator/styles/index.html', './calculator/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
